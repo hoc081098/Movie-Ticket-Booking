c3cbdf677e7efaefe089f158e1013fe79790ad33
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-14 23:57:49 +0700

    [app] :memo: UserRepository docs.

--- MobileApp/datn/lib/domain/repository/user_repository.dart
+++ MobileApp/datn/lib/domain/repository/user_repository.dart
@@ -2,6 +2,12 @@ import 'package:datn/domain/model/exception.dart';
 import 'package:tuple/tuple.dart';
 
 abstract class UserRepository {
+  /// Returns a tuple:
+  /// - item1 is true: already logged in -> home page.
+  /// - otherwise:
+  ///     - item2 is null: logged out -> login page.
+  ///     - otherwise: not completed login -> update login profile.
+  /// Maybe throw exception.
   Future<Tuple2<bool, NotCompletedLoginException>> checkAuth();
 
   Future<void> logout();
