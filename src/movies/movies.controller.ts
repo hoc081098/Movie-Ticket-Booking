5c9aaa72ed985d182cf59c5c96d4cfdef0430fb3
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-22 23:33:31 +0700

    [api] feat(movie) updated movies controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, DefaultValuePipe, Get, ParseIntPipe, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, DefaultValuePipe, Get, ParseIntPipe, Post, Query, UseGuards, Logger } from '@nestjs/common';
 import { MovieDbService } from './movie-db/movie-db.service';
 import { MoviesService } from './movies.service';
 import { Movie } from './movie.schema';
@@ -8,10 +8,12 @@ import { AuthGuard } from '../auth/auth.guard';
 @UseGuards(AuthGuard)
 @Controller('movies')
 export class MoviesController {
+  private readonly logger = new Logger('MoviesController');
+
   constructor(
-      private readonly movieDbService: MovieDbService,
-      private readonly moviesService: MoviesService,
-  ) {}
+    private readonly movieDbService: MovieDbService,
+    private readonly moviesService: MoviesService,
+  ) { }
 
   @Post('seed')
   seed(@Body() { query, page, year }: { query: string, page: number, year: number }) {
@@ -25,14 +27,20 @@ export class MoviesController {
 
   @Get('now-playing')
   async getNowShowingMovies(
-      @Query('page', new DefaultValuePipe(constants.defaultPage), ParseIntPipe) page: number,
-      @Query('per_page', new DefaultValuePipe(constants.defaultPerPage), ParseIntPipe) perPage: number,
-      @Query('lat') lat?: number,
-      @Query('lng') lng?: number,
+    @Query('page', new DefaultValuePipe(constants.defaultPage), ParseIntPipe) page: number,
+    @Query('per_page', new DefaultValuePipe(constants.defaultPerPage), ParseIntPipe) perPage: number,
+    @Query('lat') latS?: string,
+    @Query('lng') lngS?: string,
   ): Promise<Movie[]> {
-    lat = parseFloat(lat as any);
-    lng = parseFloat(lng as any);
+    this.logger.debug(`getNowShowingMovies [1]: '${latS}', '${lngS}'`);
+    if (!latS || !lngS) {
+      return this.moviesService.getNowShowingMovies(null, page, perPage);
+    }
 
+    const lat = parseFloat(latS);
+    const lng = parseFloat(lngS);
+
+    this.logger.debug(`getNowShowingMovies [1]: ${lat}, ${lng} ${isNaN(lat) || isNaN(lng)}`);
     if (isNaN(lat) || isNaN(lng)) {
       return this.moviesService.getNowShowingMovies(null, page, perPage);
     }
@@ -42,8 +50,8 @@ export class MoviesController {
 
   @Get('coming-soon')
   async getComingSoonMovies(
-      @Query('page', new DefaultValuePipe(constants.defaultPage), ParseIntPipe) page: number,
-      @Query('per_page', new DefaultValuePipe(constants.defaultPerPage), ParseIntPipe) perPage: number,
+    @Query('page', new DefaultValuePipe(constants.defaultPage), ParseIntPipe) page: number,
+    @Query('per_page', new DefaultValuePipe(constants.defaultPerPage), ParseIntPipe) perPage: number,
   ) {
     return this.moviesService.getComingSoonMovies(page, perPage);
   }
