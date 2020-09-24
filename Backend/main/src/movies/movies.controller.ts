53bea5acac9f52c07bae5f3486029a8046f0b608
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-24 15:45:28 +0700

    [api] feat(movies) movie detail

--- Backend/main/src/movies/movies.controller.ts
+++ Backend/main/src/movies/movies.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, DefaultValuePipe, Get, Logger, ParseIntPipe, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Logger,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  UseGuards
+} from '@nestjs/common';
 import { MovieDbService } from './movie-db/movie-db.service';
 import { MoviesService } from './movies.service';
 import { Movie } from './movie.schema';
 import { constants } from '../utils';
 import { AuthGuard } from '../auth/auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
-@UseGuards(AuthGuard)
+@ApiTags('movies')
+// @UseGuards(AuthGuard)
 @Controller('movies')
 export class MoviesController {
   private readonly logger = new Logger('MoviesController');
@@ -54,5 +67,12 @@ export class MoviesController {
       @Query('per_page', new DefaultValuePipe(constants.defaultPerPage), ParseIntPipe) perPage: number,
   ) {
     return this.moviesService.getComingSoonMovies(page, perPage);
+  }
+
+  @Get(':id')
+  getDetail(
+      @Param('id') id: string,
+  ): Promise<Movie | null> {
+    return this.moviesService.getDetail(id)
   }
 }
