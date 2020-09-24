abcecd392c976c0a82950b6cfb48886e19843db7
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-25 00:57:38 +0700

    [api] updated

---
+++ Backend/main/src/movies/movie.dto.ts
@@ -0,0 +1,20 @@
+import { PaginationDto } from '../pagination.dto';
+import { IsNumber, IsOptional, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import { LatLng } from '../utils';
+
+export class GetNowShowingMoviesDto extends PaginationDto implements LatLng {
+  @IsOptional()
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
+  @Type(() => Number)
+  lat?: number | null | undefined;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
+  @Type(() => Number)
+  lng?: number | null | undefined;
+}
