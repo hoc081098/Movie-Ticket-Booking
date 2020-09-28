3fd62699473df68c4bdcfe1058e64320c30fc4c0
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-28 14:17:04 +0700

    [app] :heavy_plus_sign: feat(movies) opening youtube

--- MobileApp/datn/lib/ui/home/detail/info/movie_info_page.dart
+++ MobileApp/datn/lib/ui/home/detail/info/movie_info_page.dart
@@ -1,7 +1,5 @@
 import 'package:built_collection/src/list.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:datn/domain/model/person.dart';
-import 'package:datn/ui/widgets/age_type.dart';
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_pattern/flutter_bloc_pattern.dart';
@@ -9,10 +7,14 @@ import 'package:flutter_provider/flutter_provider.dart';
 import 'package:intl/intl.dart';
 import 'package:loading_indicator/loading_indicator.dart';
 import 'package:stream_loader/stream_loader.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../domain/model/movie.dart';
+import '../../../../domain/model/person.dart';
 import '../../../../domain/repository/movie_repository.dart';
 import '../../../../utils/error.dart';
+import '../../../../utils/utils.dart';
+import '../../../widgets/age_type.dart';
 import '../../../widgets/error_widget.dart';
 
 class MovieInfoPage extends StatefulWidget {
@@ -96,143 +98,7 @@ class _MovieInfoPageState extends State<MovieInfoPage>
 
           return CustomScrollView(
             slivers: [
-              SliverAppBar(
-                expandedHeight: 300,
-                pinned: false,
-                floating: false,
-                flexibleSpace: FlexibleSpaceBar(
-                  collapseMode: CollapseMode.parallax,
-                  background: Container(
-                    color: Colors.white,
-                    child: Stack(
-                      children: <Widget>[
-                        Positioned.fill(
-                          child: CachedNetworkImage(
-                            imageUrl: movie.posterUrl ?? '',
-                            fit: BoxFit.cover,
-                            placeholder: (_, __) => Center(
-                              child: CircularProgressIndicator(
-                                strokeWidth: 2,
-                              ),
-                            ),
-                            errorWidget: (_, __, ___) => Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.error,
-                                    color: Theme.of(context).accentColor,
-                                  ),
-                                  SizedBox(height: 4),
-                                  Text(
-                                    'Load image error',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .subtitle2
-                                        .copyWith(fontSize: 12),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned.fill(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.1),
-                            ),
-                          ),
-                        ),
-                        Positioned.fill(
-                          child: Container(
-                            constraints: BoxConstraints.expand(),
-                            decoration: BoxDecoration(
-                              backgroundBlendMode: BlendMode.screen,
-                              gradient: LinearGradient(
-                                colors: <Color>[
-                                  Colors.black.withOpacity(0.5),
-                                  const Color(0xff545AE9).withOpacity(0.6),
-                                  const Color(0xffB881F9),
-                                ],
-                                stops: [0, 0.5, 1],
-                                begin: AlignmentDirectional.topEnd,
-                                end: AlignmentDirectional.bottomStart,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned.fill(
-                          child: Container(
-                            constraints: BoxConstraints.expand(),
-                            decoration: BoxDecoration(
-                              backgroundBlendMode: BlendMode.multiply,
-                              gradient: LinearGradient(
-                                colors: <Color>[
-                                  const Color(0xff545AE9).withOpacity(0.8),
-                                  const Color(0xffB881F9).withOpacity(0.8),
-                                ],
-                                begin: AlignmentDirectional.topEnd,
-                                end: AlignmentDirectional.bottomStart,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          top: 16,
-                          right: 16,
-                          child: Material(
-                            color: Colors.transparent,
-                            child: Row(
-                              children: [
-                                IconButton(
-                                  icon: Icon(
-                                    Icons.share,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {},
-                                ),
-                                const SizedBox(width: 8),
-                                IconButton(
-                                  icon: Icon(
-                                    Icons.favorite,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {},
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: AlignmentDirectional.center,
-                          child: InkWell(
-                            onTap: () {},
-                            child: Container(
-                              width: 60,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(30),
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 2,
-                                ),
-                              ),
-                              child: Center(
-                                child: Icon(
-                                  Icons.play_arrow,
-                                  color: Colors.white,
-                                  size: 36,
-                                ),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                automaticallyImplyLeading: false,
-              ),
+              DetailAppBar(movie: movie),
               SliverToBoxAdapter(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -359,6 +225,162 @@ class _MovieInfoPageState extends State<MovieInfoPage>
 
   @override
   bool get wantKeepAlive => true;
+}
+
+class DetailAppBar extends StatelessWidget {
+  const DetailAppBar({
+    Key key,
+    @required this.movie,
+  }) : super(key: key);
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 300,
+      pinned: false,
+      floating: false,
+      flexibleSpace: FlexibleSpaceBar(
+        collapseMode: CollapseMode.parallax,
+        background: Container(
+          color: Colors.white,
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: CachedNetworkImage(
+                  imageUrl: movie.posterUrl ?? '',
+                  fit: BoxFit.cover,
+                  placeholder: (_, __) => Center(
+                    child: CircularProgressIndicator(
+                      strokeWidth: 2,
+                    ),
+                  ),
+                  errorWidget: (_, __, ___) => Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.error,
+                          color: Theme.of(context).accentColor,
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          'Load image error',
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle2
+                              .copyWith(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.1),
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                child: Container(
+                  constraints: BoxConstraints.expand(),
+                  decoration: BoxDecoration(
+                    backgroundBlendMode: BlendMode.screen,
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Colors.black.withOpacity(0.5),
+                        const Color(0xff545AE9).withOpacity(0.6),
+                        const Color(0xffB881F9),
+                      ],
+                      stops: [0, 0.5, 1],
+                      begin: AlignmentDirectional.topEnd,
+                      end: AlignmentDirectional.bottomStart,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                child: Container(
+                  constraints: BoxConstraints.expand(),
+                  decoration: BoxDecoration(
+                    backgroundBlendMode: BlendMode.multiply,
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        const Color(0xff545AE9).withOpacity(0.8),
+                        const Color(0xffB881F9).withOpacity(0.8),
+                      ],
+                      begin: AlignmentDirectional.topEnd,
+                      end: AlignmentDirectional.bottomStart,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 16,
+                right: 16,
+                child: Material(
+                  color: Colors.transparent,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.share,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(width: 8),
+                      IconButton(
+                        icon: Icon(
+                          Icons.favorite,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional.center,
+                child: InkWell(
+                  onTap: () async {
+                    if (await canLaunch(movie.trailerVideoUrl)) {
+                      await launch(movie.trailerVideoUrl);
+                    } else {
+                      context.showSnackBar('Cannot open trailer video');
+                    }
+                  },
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.play_arrow,
+                        color: Colors.white,
+                        size: 36,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      automaticallyImplyLeading: false,
+    );
+  }
 }
 
 class PeopleList extends StatelessWidget {
