ba15750616596fc1a785c760a4e5b195098d30ea
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-28 17:46:20 +0700

    [api] feat(seats) add seats

---
+++ src/seats/seat.schema.ts
@@ -0,0 +1,35 @@
+import { Document, Schema as MongooseSchema } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  collection: 'seats',
+  timestamps: true,
+})
+export class Seat extends Document {
+  @Prop({ required: true })
+  row: string;
+
+  @Prop({ required: true })
+  column: number;
+
+  @Prop({ required: true })
+  count: number;
+
+  @Prop({
+    type: [Number],
+    required: true,
+  })
+  coordinates: [number, number];
+
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: 'Theatre',
+    required: true,
+  })
+  theatre: any;
+
+  @Prop({ default: true })
+  is_active: boolean;
+}
+
+export const SeatSchema = SchemaFactory.createForClass(Seat);
---
+++ src/seats/seats.service.ts
@@ -0,0 +1,79 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DocumentDefinition, Model } from 'mongoose';
+import { Seat } from './seat.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Theatre } from '../theatres/theatre.schema';
+
+@Injectable()
+export class SeatsService {
+  constructor(
+      @InjectModel(Seat.name) private readonly seatModel: Model<Seat>,
+      @InjectModel(Theatre.name) private readonly theatreModel: Model<Seat>,
+  ) {}
+
+  async seed(id: string): Promise<Seat[]> {
+    const theatre = await this.theatreModel.findById(id);
+    if (theatre == null) {
+      throw new NotFoundException();
+    }
+
+    await this.seatModel.deleteMany({ theatre: theatre._id });
+
+    const start = 'A'.charCodeAt(0);
+    const end = 'H'.charCodeAt(0);
+    const seats: Seat[] = [];
+
+    for (let row = 0; row <= end - start; row++) {
+      let currentCol = 1;
+
+      if (row < end - start - 1) {
+        for (let col = 2; col <= 11; col++) {
+          const doc: Omit<DocumentDefinition<Seat>, '_id'> = {
+            theatre: theatre._id,
+            column: currentCol++,
+            row: String.fromCharCode(start + row),
+            coordinates: [col, row],
+            count: 1,
+            is_active: true,
+          };
+
+          seats.push(await this.seatModel.create(doc));
+        }
+      } else {
+        for (let col = 0; col <= 5; col += 2) {
+          const doc: Omit<DocumentDefinition<Seat>, '_id'> = {
+            theatre: theatre._id,
+            column: currentCol++,
+            row: String.fromCharCode(start + row),
+            coordinates: [col, row],
+            count: 2,
+            is_active: true,
+          };
+
+          seats.push(await this.seatModel.create(doc));
+        }
+
+        for (let col = 6; col <= 14; col++) {
+          const doc: Omit<DocumentDefinition<Seat>, '_id'> = {
+            theatre: theatre._id,
+            column: currentCol++,
+            row: String.fromCharCode(start + row),
+            coordinates: [col, row],
+            count: 1,
+            is_active: true,
+          };
+
+          seats.push(await this.seatModel.create(doc));
+        }
+
+      }
+
+    }
+
+    return seats;
+  }
+
+  getSeatsByTheatreId(theatreId: string): Promise<Seat[]> {
+    return this.seatModel.find({ theatre: theatreId }).exec();
+  }
+}
