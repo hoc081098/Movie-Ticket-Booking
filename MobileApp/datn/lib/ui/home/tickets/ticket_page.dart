cd194f1ba0bcfd0706ef980fd018fd50888c9e8c
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-30 23:15:25 +0700

    [app] :heavy_plus_sign: feat(tickets) UI init

--- MobileApp/datn/lib/ui/home/tickets/ticket_page.dart
+++ MobileApp/datn/lib/ui/home/tickets/ticket_page.dart
@@ -120,6 +120,7 @@ class _TicketsPageState extends State<TicketsPage> with DisposeBagMixin {
                   selectedTicketIdsS.add(newIds);
                 },
               ),
+              LegendsWidget()
             ],
           ),
         );
@@ -414,6 +415,115 @@ class SeatWidget extends StatelessWidget {
                   color: isSelected ? Colors.white : const Color(0xff687189),
                 ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class LegendsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final widthPerSeat = MediaQuery.of(context).size.width / 12;
+    final accentColor = Theme.of(context).accentColor;
+    final textStyle = Theme.of(context).textTheme.subtitle2.copyWith(
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        );
+
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 16,
+          runSpacing: 12,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(0.5),
+                  width: widthPerSeat,
+                  height: widthPerSeat,
+                  decoration: BoxDecoration(
+                    color: accentColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                const SizedBox(width: 6),
+                Text(
+                  'Select',
+                  style: textStyle,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(0.5),
+                  width: widthPerSeat,
+                  height: widthPerSeat,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: const Color(0xffCBD7E9),
+                      width: 1,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 6),
+                Text(
+                  'Available',
+                  style: textStyle,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(0.5),
+                  width: widthPerSeat,
+                  height: widthPerSeat,
+                  decoration: BoxDecoration(
+                    color: const Color(0xffCBD7E9),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                const SizedBox(width: 6),
+                Text(
+                  'Taken',
+                  style: textStyle,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(0.5),
+                  width: widthPerSeat * 2,
+                  height: widthPerSeat,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: const Color(0xffCBD7E9),
+                      width: 1,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 6),
+                Text(
+                  'Duplicated seat',
+                  style: textStyle,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
