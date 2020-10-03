9ff9d00dd764004ec480a42a3f2817073648c759
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-10-03 16:33:30 +0700

    [api] stripe cards

---
+++ src/users/cards/card.dto.ts
@@ -0,0 +1,53 @@
+import { IsNotEmpty, IsNumber, IsString, Length, Matches, Max, Min } from 'class-validator';
+import dayjs = require('dayjs');
+
+export class Card {
+  readonly id: string;
+  readonly brand: string;
+  readonly country: string;
+  readonly exp_month: number;
+  readonly exp_year: number;
+  readonly funding: string;
+  readonly last4: string;
+  readonly card_holder_name: string;
+
+  constructor(raw: {
+    id: string,
+    brand: string,
+    country: string,
+    exp_month: number,
+    exp_year: number,
+    funding: string,
+    last4: string,
+    card_holder_name: string
+  }) {
+    Object.assign(this, raw);
+  }
+}
+
+export class AddCardDto {
+
+  @IsString()
+  @IsNotEmpty()
+  readonly card_holder_name: string;
+
+  @IsString()
+  @Length(16, 16)
+  @Matches(/^[0-9]{16}$/)
+  readonly number: string;
+
+  @IsNumber()
+  @Min(dayjs().year() % 100)
+  @Max(99)
+  readonly exp_year: number;
+
+  @IsNumber()
+  @Min(1)
+  @Max(12)
+  readonly exp_month: number;
+
+  @IsString()
+  @Length(3, 3)
+  @Matches(/^[0-9]{3}$/)
+  readonly cvc: string;
+}
--- src/users/cards/cards.controller.ts
+++ src/users/cards/cards.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../../auth/auth.guard';
 import { GetUser, UserPayload } from '../../auth/get-user.decorator';
-import { Card } from '../card.dto';
+import { AddCardDto, Card } from './card.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { UsersService } from '../users.service';
 
@@ -18,5 +18,24 @@ export class CardsController {
       @GetUser() user: UserPayload,
   ): Promise<Card[]> {
     return this.usersService.getCards(user);
+  }
+
+  @Post()
+  addCard(
+      @GetUser() userPayload: UserPayload,
+      @Body() cardDto: AddCardDto
+  ) {
+    return this.usersService.addCard(userPayload, cardDto);
+  }
+
+  @Delete(':id')
+  removeCard(
+      @GetUser() userPayload: UserPayload,
+      @Param('id') cardId: string
+  ): Promise<'SUCCESS'> {
+    return this.usersService.removeCard(
+        userPayload,
+        cardId
+    );
   }
 }
