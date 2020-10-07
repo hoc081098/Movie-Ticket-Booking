cbf634a6fb88eabe48599211e5267c159080e46c
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-10-07 15:35:09 +0700

    [api] done add card

--- Backend/main/src/users/cards/cards.controller.ts
+++ Backend/main/src/users/cards/cards.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../../auth/auth.guard';
 import { GetUser, UserPayload } from '../../auth/get-user.decorator';
 import { AddCardDto, Card } from './card.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { UsersService } from '../users.service';
+import dayjs = require('dayjs');
 
 @ApiTags('cards')
 @UseGuards(AuthGuard)
@@ -25,6 +26,18 @@ export class CardsController {
       @GetUser() userPayload: UserPayload,
       @Body() cardDto: AddCardDto
   ) {
+    const current = dayjs();
+    const currentYear = current.year() % 100;
+
+    if (cardDto.exp_year < currentYear) {
+      throw new BadRequestException('Invalid exp_year. exp_year must be equal to or greater than current year.');
+    }
+
+    const currentMonth = current.month() + 1;
+    if (cardDto.exp_year === currentYear && cardDto.exp_month < currentMonth) {
+      throw new BadRequestException('Invalid exp_month. exp_month must be equal to or greater than current month.');
+    }
+
     return this.usersService.addCard(userPayload, cardDto);
   }
 
