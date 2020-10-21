ac4bbec0c20ae0c321aeb1884f7645b973b2c15e
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-10-21 16:09:46 +0700

    [app]

--- MobileApp/datn/lib/ui/notifications/notifications_page.dart
+++ MobileApp/datn/lib/ui/notifications/notifications_page.dart
@@ -1,39 +1,178 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Notification, Action;
+import 'package:flutter_disposebag/flutter_disposebag.dart';
 import 'package:flutter_provider/flutter_provider.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+import 'package:rx_redux/rx_redux.dart';
 
 import '../../domain/repository/notification_repository.dart';
-import '../../utils/streams.dart';
+import '../../utils/error.dart';
+import '../../utils/utils.dart';
 import '../app_scaffold.dart';
+import '../widgets/empty_widget.dart';
+import '../widgets/error_widget.dart';
+import 'action.dart';
+import 'state.dart' as st;
+import 'store.dart';
 
 class NotificationsPage extends StatefulWidget {
   @override
   _NotificationsPageState createState() => _NotificationsPageState();
 }
 
-class _NotificationsPageState extends State<NotificationsPage> {
+class _NotificationsPageState extends State<NotificationsPage>
+    with DisposeBagMixin {
+  RxReduxStore<Action, st.State> store;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
 
-    AppScaffold.tapStream(context)
-    .where((event) => event == 2)
-    .take(1)
-    .listen((event) {
-      Provider.of<NotificationRepository>(context)
-          .getNotification(page: 1, perPage: 32)
-          .debug('>> NOTIFICATION')
-          .listen(null);
-    });
+    store ??= () {
+      final s = createStore(
+          Provider.of<NotificationRepository>(context).getNotification);
+
+      subscribe(s);
+
+      AppScaffold.tapStream(context)
+          .where((event) => event == 2)
+          .take(1)
+          .listen((_) => s.dispatch(const LoadFirstPageAction()));
+
+      return s;
+    }();
+  }
+
+  void subscribe(RxReduxStore<Action, st.State> store) {
+    store.stateStream
+        .listen((state) => print('Page: ${state.page}'))
+        .disposedBy(bag);
+
+    store.actionStream.listen((action) {
+      if (action is FailureAction) {
+        scaffoldKey.showSnackBar(
+          'Error occurred: ${getErrorMessage(action.error)}',
+        );
+      }
+      if (action is SuccessAction) {
+        if (action.notifications.isEmpty) {
+          scaffoldKey.showSnackBar('Loaded all notifications');
+        }
+      }
+    }).disposedBy(bag);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text('Notifications'),
       ),
-      body: Center(
-        child: Text('Hi'),
+      body: StreamBuilder<st.State>(
+        stream: store.stateStream,
+        initialData: store.state,
+        builder: (context, snapshot) {
+          final state = snapshot.data;
+
+          if (state.isLoading && state.isFirstPage) {
+            return Center(
+              child: SizedBox(
+                width: 56,
+                height: 56,
+                child: LoadingIndicator(
+                  color: Theme.of(context).accentColor,
+                  indicatorType: Indicator.ballScaleMultiple,
+                ),
+              ),
+            );
+          }
+
+          if (state.error != null && state.isFirstPage) {
+            return Center(
+              child: MyErrorWidget(
+                errorText: 'Error: ${getErrorMessage(state.error)}',
+                onPressed: () => store.dispatch(const RetryAction()),
+              ),
+            );
+          }
+
+          if (state.items.isEmpty) {
+            return Center(
+              child: EmptyWidget(
+                message: 'Empty comments',
+              ),
+            );
+          }
+
+          final items = state.items;
+
+          return ListView.separated(
+            itemCount: items.length + (state.isFirstPage ? 0 : 1),
+            itemBuilder: (context, index) {
+              if (index < items.length) {
+                final item = items[index];
+
+                return ListTile(
+                  title: Text(item.reservation.showTime.movie.title),
+                );
+              }
+
+              if (state.error != null) {
+                return Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: MyErrorWidget(
+                    errorText:
+                        'Load page ${state.page}, error: ${getErrorMessage(state.error)}',
+                    onPressed: () => store.dispatch(const RetryAction()),
+                  ),
+                );
+              }
+
+              if (state.isLoading) {
+                return Center(
+                  child: SizedBox(
+                    width: 56,
+                    height: 56,
+                    child: LoadingIndicator(
+                      color: Theme.of(context).accentColor,
+                      indicatorType: Indicator.ballScaleMultiple,
+                    ),
+                  ),
+                );
+              }
+
+              if (state.loadedAll) {
+                return const SizedBox(width: 0, height: 0);
+              }
+
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 12),
+                child: Center(
+                  child: SizedBox(
+                    width: 128,
+                    height: 48,
+                    child: RaisedButton(
+                      onPressed: () =>
+                          store.dispatch(const LoadNextPageAction()),
+                      child: Text('Next page'),
+                      elevation: 8,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        side: BorderSide(
+                          color: Theme.of(context).primaryColor,
+                          width: 1,
+                        ),
+                      ),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const Divider(),
+          );
+        },
       ),
     );
   }
---
+++ MobileApp/datn/lib/ui/notifications/store.dart
@@ -0,0 +1,78 @@
+import 'package:built_collection/built_collection.dart';
+
+import '../../domain/model/notification.dart';
+import 'package:meta/meta.dart';
+import 'package:rx_redux/rx_redux.dart';
+import 'package:rxdart/rxdart.dart' hide Notification;
+
+import 'action.dart';
+import 'state.dart';
+
+typedef GetNotifications = Stream<BuiltList<Notification>> Function({
+  @required int page,
+  @required int perPage,
+});
+
+const perPage = 32;
+
+RxReduxStore<Action, State> createStore(
+  GetNotifications getNotifications,
+) =>
+    RxReduxStore(
+      initialState: State.initial(),
+      sideEffects: SideEffects(getNotifications)(),
+      reducer: (state, action) => action.reduce(state),
+      // logger: rxReduxDefaultLogger,
+    );
+
+class SideEffects {
+  final GetNotifications getNotifications;
+
+  SideEffects(this.getNotifications);
+
+  List<SideEffect<Action, State>> call() => [
+        firstPage,
+        nextPage,
+        retry,
+      ];
+
+  Stream<Action> firstPage(
+    Stream<Action> actions,
+    GetState<State> getState,
+  ) =>
+      actions
+          .whereType<LoadFirstPageAction>()
+          .take(1)
+          .exhaustMap((_) => _nextPage(1));
+
+  Stream<Action> nextPage(
+    Stream<Action> actions,
+    GetState<State> getState,
+  ) {
+    return actions
+        .whereType<LoadNextPageAction>()
+        .map((_) => getState())
+        .where((state) => state.canLoadNextPage)
+        .exhaustMap((state) => _nextPage(state.page + 1));
+  }
+
+  Stream<Action> retry(
+    Stream<Action> actions,
+    GetState<State> getState,
+  ) {
+    return actions
+        .whereType<RetryAction>()
+        .map((_) => getState())
+        .where((state) => state.canRetry)
+        .exhaustMap((state) => _nextPage(state.page + 1));
+  }
+
+  Stream<Action> _nextPage(int nextPage) {
+    final loadingAction = LoadingAction(nextPage);
+
+    return getNotifications(page: nextPage, perPage: perPage)
+        .map<Action>((items) => SuccessAction(items))
+        .startWith(loadingAction)
+        .onErrorReturnWith((error) => FailureAction(error));
+  }
+}
