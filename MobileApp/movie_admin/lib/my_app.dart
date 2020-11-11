6da7a3bbc953e59131637dc15153df6aec18a58c
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-11-11 16:22:30 +0700

    [app] update(deps)

--- MobileApp/movie_admin/lib/my_app.dart
+++ MobileApp/movie_admin/lib/my_app.dart
@@ -85,7 +85,7 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-  static const minSplashDuration = Duration(seconds: 2, milliseconds: 500);
+  static const minSplashDuration = Duration(seconds: 2);
   Future<AuthState> checkAuthFuture;
 
   @override
@@ -94,11 +94,11 @@ class _SplashPageState extends State<SplashPage> {
 
     checkAuthFuture ??= () async {
       final stopwatch = Stopwatch()..start();
-      final authState = Provider.of<UserRepository>(context).checkAuth();
+      final authState = await Provider.of<UserRepository>(context).checkAuth();
       stopwatch.stop();
 
       final extraDelay = minSplashDuration - stopwatch.elapsed;
-      print('>> extraDelay=$extraDelay');
+      print('>> extraDelay=${extraDelay.inMilliseconds}');
       if (extraDelay > Duration.zero) {
         await Future.delayed(extraDelay);
       }
