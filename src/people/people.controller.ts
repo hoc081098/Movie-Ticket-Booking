3e21ab32c3278f989115ea699beb6183fdbe9417
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-11-12 23:06:46 +0700

    [api] admin add movie

--- src/people/people.controller.ts
+++ src/people/people.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { AuthGuard } from '../auth/auth.guard';
 import { ApiTags } from '@nestjs/swagger';
+import { Person } from './person.schema';
 
 @ApiTags('people')
 @UseGuards(AuthGuard)
@@ -11,4 +12,10 @@ export class PeopleController {
       private readonly peopleService: PeopleService,
   ) {}
 
+  @Get('search')
+  searchByName(
+      @Query('name') name: string,
+  ): Promise<Person[]> {
+    return this.peopleService.searchByName(name);
+  }
 }
