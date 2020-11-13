d8ccf3d71ee869b65a908f0fc68662355ff2b5d1
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-11-13 16:30:18 +0700

    [api] add theatres + seats [init]

---
+++ src/theatres/theatre.dto.ts
@@ -0,0 +1,73 @@
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  Min,
+  ValidateNested,
+} from "class-validator";
+import { LocationDto } from "../common/location.dto";
+
+export class AddTheatreDto extends LocationDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsString()
+  @IsPhoneNumber('VN')
+  phone_number: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email?: string | null;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  thumbnail?: string | null;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  cover?: string | null;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested()
+  seats: SeatDto[];
+}
+
+export class SeatDto {
+  @IsString()
+  @IsNotEmpty()
+  row: string;
+
+  @IsNumber()
+  @Min(1)
+  count: number;
+
+  @IsArray()
+  @IsNumber({}, { each: true })
+  @ArrayMinSize(2)
+  @ArrayMaxSize(2)
+  coordinates: [number, number];
+
+  @IsString()
+  @IsNotEmpty()
+  theatre: string;
+}
--- src/theatres/theatres.module.ts
+++ src/theatres/theatres.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { TheatresController } from './theatres.controller';
+import { AdminTheatresController, TheatresController } from './theatres.controller';
 import { TheatresService } from './theatres.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Theatre, TheatreSchema } from './theatre.schema';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { ConfigModule } from '../config/config.module';
+import { Seat, SeatSchema } from "../seats/seat.schema";
 
 @Module({
   imports: [
@@ -14,12 +15,16 @@ import { ConfigModule } from '../config/config.module';
         name: Theatre.name,
         schema: TheatreSchema,
       },
+      {
+        name: Seat.name,
+        schema: SeatSchema,
+      },
     ]),
     AuthModule,
     UsersModule,
     ConfigModule,
   ],
-  controllers: [TheatresController],
+  controllers: [TheatresController, AdminTheatresController],
   providers: [TheatresService]
 })
 export class TheatresModule {}
