3d561e2c6e14852713f240a290a88c99013f4948
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-12-21 09:17:35 +0700

    interactive union

--- Backend/main/src/neo4j/neo4j.service.ts
+++ Backend/main/src/neo4j/neo4j.service.ts
@@ -651,6 +651,31 @@ export class Neo4jService {
           WITH sum(score) AS score, rec, cat, distance
           RETURN rec._id AS _id, score AS recommendation, null AS pearson, score, collect(DISTINCT cat.name) AS cats, distance
           ORDER BY recommendation DESC LIMIT 24
+          
+          UNION ALL
+          
+          MATCH (u1:USER { _id: $id })-[r1:INTERACTIVE]->(m:MOVIE)
+          WITH u1, avg(r1.score) AS u1_mean
+          
+          MATCH (u1)-[r1:INTERACTIVE]->(m:MOVIE)<-[r2:INTERACTIVE]-(u2:USER)
+          WITH u1, u1_mean, u2, collect({ r1: r1, r2: r2 }) AS iteractions WHERE size(iteractions) > 1
+          
+          MATCH (u2)-[r2:INTERACTIVE]->(m:MOVIE)
+          WITH u1, u1_mean, u2, avg(r2.score) AS u2_mean, iteractions
+          
+          UNWIND iteractions AS r
+          
+          WITH sum( (r.r1.score - u1_mean) * (r.r2.score - u2_mean) ) AS nom,
+               sqrt( sum((r.r1.score - u1_mean) ^ 2) * sum((r.r2.score - u2_mean) ^ 2) ) AS denom,
+               u1, u2 WHERE denom <> 0
+            
+          WITH u1, u2, nom / denom AS pearson
+          ORDER BY pearson DESC LIMIT 15
+          
+          MATCH (u2)-[r:INTERACTIVE]->(m:MOVIE) WHERE NOT exists( (u1)-[:INTERACTIVE]->(m) )
+          WITH m, pearson, r
+          RETURN m._id AS _id, sum(pearson * r.score) AS recommendation, pearson, r.score AS score, null AS cats, null as distance
+          ORDER BY recommendation DESC LIMIT 24
           `,
           {
             id: user._id.toString(),
@@ -711,6 +736,31 @@ export class Neo4jService {
           
           WITH sum(score) AS score, rec, cat
           RETURN rec._id AS _id, score AS recommendation, null AS pearson, score, collect(DISTINCT cat.name) AS cats
+          ORDER BY recommendation DESC LIMIT 24
+          
+          UNION ALL
+          
+          MATCH (u1:USER { _id: $id })-[r1:INTERACTIVE]->(m:MOVIE)
+          WITH u1, avg(r1.score) AS u1_mean
+          
+          MATCH (u1)-[r1:INTERACTIVE]->(m:MOVIE)<-[r2:INTERACTIVE]-(u2:USER)
+          WITH u1, u1_mean, u2, collect({ r1: r1, r2: r2 }) AS iteractions WHERE size(iteractions) > 1
+          
+          MATCH (u2)-[r2:INTERACTIVE]->(m:MOVIE)
+          WITH u1, u1_mean, u2, avg(r2.score) AS u2_mean, iteractions
+          
+          UNWIND iteractions AS r
+          
+          WITH sum( (r.r1.score - u1_mean) * (r.r2.score - u2_mean) ) AS nom,
+               sqrt( sum((r.r1.score - u1_mean) ^ 2) * sum((r.r2.score - u2_mean) ^ 2) ) AS denom,
+               u1, u2 WHERE denom <> 0
+            
+          WITH u1, u2, nom / denom AS pearson
+          ORDER BY pearson DESC LIMIT 15
+          
+          MATCH (u2)-[r:INTERACTIVE]->(m:MOVIE) WHERE NOT exists( (u1)-[:INTERACTIVE]->(m) )
+          WITH m, pearson, r
+          RETURN m._id AS _id, sum(pearson * r.score) AS recommendation, pearson, r.score AS score, null AS cats
           ORDER BY recommendation DESC LIMIT 24
           `,
           {
